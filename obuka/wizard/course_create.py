b4cb8c8d17f127610e39e8ed1333da0133b54f00
Damjan <gdamjan@gmail.com>
2012-02-24 10:14:21 +0100

    Complete the wizard code
    the model got columns and methods
    added an xml file with a form and action window entry
    the required __init__.py was created too

--- obuka/wizard/course_create.py
+++ obuka/wizard/course_create.py
@@ -4,6 +4,28 @@ class course_create(osv.osv_memory):
     _name = 'obuka.course.create'
     _description = 'Create courses'
 
+    _columns = {
+        'course_number': fields.integer('Number of courses', required=True),
+    }
+
+    _defaults = {
+        'course_number': 5,
+    }
+
+    def create_courses(self, cr, uid, ids, context=None):
+        course_obj = self.pool.get('obuka.course')
+        course_number = self.browse(cr, uid, ids[0], context).course_number
+
+        for i in range(course_number):
+            num = i + 1
+            new_course = {
+                'name':'Course %d' % num,
+                'description': 'Course created by wizard with poz %d' % num,
+                'session_id': context['active_id']
+            }
+            course_obj.create(cr, uid, new_course)
+
+        return {'type':'ir.actions.act_window_close'}
 
 
 course_create()
