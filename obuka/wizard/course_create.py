ef3565bd51a9ff596fb319c71a06c12ca73adc83
Damjan <gdamjan@gmail.com>
2012-02-24 10:35:00 +0100

    Add instructor selector in the wizard

--- obuka/wizard/course_create.py
+++ obuka/wizard/course_create.py
@@ -6,6 +6,7 @@ class course_create(osv.osv_memory):
 
     _columns = {
         'course_number': fields.integer('Number of courses', required=True),
+        'instructor_id': fields.many2one('res.partner', 'Instructor', required=True)
     }
 
     _defaults = {
@@ -15,13 +16,15 @@ class course_create(osv.osv_memory):
     def create_courses(self, cr, uid, ids, context=None):
         course_obj = self.pool.get('obuka.course')
         course_number = self.browse(cr, uid, ids[0], context).course_number
+        instructor = self.browse(cr, uid, ids[0], context).instructor_id.id
 
         for i in range(course_number):
             num = i + 1
             new_course = {
                 'name':'Course %d' % num,
                 'description': 'Course created by wizard with poz %d' % num,
-                'session_id': context['active_id']
+                'session_id': context['active_id'],
+                'instructor_id': instructor
             }
             course_obj.create(cr, uid, new_course)
 
